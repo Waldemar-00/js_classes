98dc6ca66d6e24e64d25847ae2165e7b387d240c
ULADZIMIR <01myit@gmail.com>
2025-01-29 21:14:41 +0300

    Override.

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ class Book
         this.title = title
         this.author = author
     }
-    buy() { console.log('BUY') }
+    buy () { console.log( 'BUY' ) }
+    info() { console.log(`${this.title} - ${this.author}` ) }
 }
+const book = new Book( 'Lord of the Rings', 'Tolkien' )
+book.info()
 
 class AudioBook extends Book
 {
@@ -23,6 +26,21 @@ class AudioBook extends Book
 
 const audiobook = new AudioBook( 'Lord of the Rings', 'Tolkien', 21 * 60 )
 
-audiobook.log()
-audiobook.buy()
-console.log( audiobook )
+// audiobook.log()
+// audiobook.buy()
+// console.log( audiobook )
+
+class Ebook extends Book
+{
+    constructor ( title, author, size )
+    {
+        super( title, author )
+        this.size = size
+    }
+    info ()
+    {
+        console.log( `${ this.title }: ${ this.author }. It size is: ${this.size}mb` ) //! OVERRIDE
+    }
+}
+const ebook = new Ebook( 'Lord of the Rings', 'Tolkien', 1020 )
+ebook.info()
